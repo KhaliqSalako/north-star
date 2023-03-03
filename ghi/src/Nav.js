3c91e31da8e1bcd97426b4e3a3fafa166562cf67
Louis <pandaboy9902@gmail.com>
2023-03-03 15:05:52 -0500

    cards functioning

--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -2,79 +2,92 @@ import { NavLink } from "react-router-dom";
 import LogOut from "./Accounts/LogOut";
 import { useToken, getToken } from "./Accounts/auth";
 
-
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-gradient-to-l from-blue to-gray-dark nav-sticky">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <div id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="mt-3">
-              <NavLink
-                className="nav-item text-2xl font-['Orbitron']"
-                to="/"
-              >
-                North Star
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/login"
-                className="nav-item"
-                role="button"
-              >
-                Login
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <LogOut />
-            </li>
-            <li>
-              <NavLink
-                className="nav-item"
-                to="/signup"
-                role="button"
-              >
-                Sign Up
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                className="nav-item"
-                to="/trips"
-                role="button"
-              >
-                Trips
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                className="nav-item"
-                to="/trips/create"
-                role="button"
-              >
-                Create a Trip
-              </NavLink>
-            </li>
-          </ul>
+            <NavLink class="navbar-brand" to="/">
+      <img src="../public/north_star_logo.jpg" alt="Logo" width="30" height="24" className="d-inline-block align-text-top">
+      Bootstrap
+    </Navlink>
+        <div className="navbar-header">
+          <NavLink className="nav-item text-3xl font-['Orbitron']" to="/">
+            North Star
+          </NavLink>
         </div>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
-        </div>
+        <ul className="nav navbar-nav navbar-right">
+          <li>
+            <NavLink className="nav-item" to="/trips">
+              Trips
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="nav-item" to="/signup">
+              Sign Up
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/login" className="nav-item">
+              Login
+            </NavLink>
+          </li>
+
+          <li className="nav-item">
+            <LogOut />
+          </li>
+        </ul>
       </div>
     </nav>
+
   );
 }
 
 export default Nav;
+
+
+
+    {/* // <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    //   <div className="container-fluid">
+    //     <div>
+    //       <ul className="navbar-nav">
+    //         <li className="mt-3">
+    //           <NavLink className="nav-item text-2xl font-['Orbitron']" to="/">
+    //             North Star
+    //           </NavLink>
+    //         </li>
+    //         <li>
+    //           <NavLink to="/login" className="nav-item">
+    //             Login
+    //           </NavLink>
+    //         </li>
+    //         <li className="nav-item">
+    //           <LogOut />
+    //         </li>
+    //         <li>
+    //           <NavLink className="nav-item" to="/signup">
+    //             Sign Up
+    //           </NavLink>
+    //         </li>
+    //         <li className="nav-item">
+    //           <NavLink className="nav-item" to="/trips">
+    //             Trips
+    //           </NavLink>
+    //         </li>
+    //       </ul>
+    //     </div>
+    //     <button
+    //       className="navbar-toggler"
+    //       type="button"
+    //       data-bs-toggle="collapse"
+    //       data-bs-target="#navbarSupportedContent"
+    //       aria-controls="navbarSupportedContent"
+    //       aria-expanded="false"
+    //       aria-label="Toggle navigation"
+    //     >
+    //       <span className="navbar-toggler-icon"></span>
+    //     </button>
+    //     <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    //       <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+    //     </div>
+    //   </div>
+    // </nav> */}
--- ghi/src/Trips/TripList.js
+++ ghi/src/Trips/TripList.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useAuthContext } from "../Accounts/auth";
 import TripListCard from "./TripListCard";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 function TripList() {
   const { token } = useAuthContext();
@@ -26,34 +26,45 @@ function TripList() {
   }, []);
 
   const deleteTrip = async (trip_id) => {
-    const response = await fetch(`${process.env.REACT_APP_ACCOUNTS_HOST}/api/trips/${trip_id}`, {
-      method: 'DELETE',
-      credentials: "include",
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json"
+    const response = await fetch(
+      `${process.env.REACT_APP_ACCOUNTS_HOST}/api/trips/${trip_id}`,
+      {
+        method: "DELETE",
+        credentials: "include",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
       }
-    })
-    const data = await response.json()
-    getTripData()
-  }
+    );
+    const data = await response.json();
+    getTripData();
+  };
 
   return (
     <>
-    <h1>Your Trips</h1>
-    <div className="container">
-      <div className="row">
-      {trips.map((trip) => {
-        return (
-          <div className="col-3 m-3">
-          <TripListCard getTripData={getTripData} trip={trip}/>
-          </div>
-        )
-      })}
+      <Link
+        className="text-self-right btn btn-primary"
+        to={`/trips/create`}
+        role="button"
+      >
+        Create a Trip
+      </Link>
+      <h1 className="text-3xl center" >Your Trips</h1>
+      <div className="container">
+        <div className="row">
+          {trips.map((trip) => {
+            return (
+              <div key={trip.id} className="col-3 m-3">
+                <TripListCard getTripData={getTripData} trip={trip} />
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>;
+      ;
     </>
-  )
+  );
 }
 
 export default TripList;
