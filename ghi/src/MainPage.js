993784618edd019c2396ca9088d76ba5ab48798e
Anri Astora <anri.astora1001@gmail.com>
2023-03-06 16:30:28 -0500

    navbar and main page stuff

--- ghi/src/MainPage.js
+++ ghi/src/MainPage.js
@@ -2,49 +2,22 @@ import React, { useEffect, useState } from "react";
 import Tripsidebar from "./Trips/Tripsidebar";
 
 function MainPage() {
-  // const [token, setToken] = useState("");
-
-  // const getTokenData = async () => {
-  //   const request = await fetch(`http://localhost:8000/token`, {
-  //     headers: { Authorization: `Bearer ${token}` },
-  //     // Other fetch options, like method and body, if applicable
-  //   });
-  //     if (request.ok) {
-  //       const data = await request.json();
-  //       setToken(data);
-  //       console.log(data)
-  //     }
-
-  //   useEffect(() => {
-  //     getTokenData();
-  //   }, []);
-
-  //   var getToken = async () => {
-  //   const response = await fetch(
-  //     `http://localhost:8000/token`
-  //   );
-  //   if (response.ok) {
-  //     const data = await response.json();
-  //     setToken(data);
-  //     console.log(data)
-  //     console.log(token)
-  //   }
-  // };
 
   return (
-    <>
-      <div className="px-4 py-5 my-5 text-center">
-        <h1 className="display-5 fw-bold font-['Orbitron']">North Star</h1>
+      <div 
+        className="text-center bg-black vh-100 vw-100"
+        style={{
+          backgroundImage:`url(background.jpg)`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: "cover"
+        }}
+      >
+        <h1 className="pt-5 fw-bold font-['Orbitron']">North Star</h1>
         <div className="col-lg-6 mx-auto">
           <p className="lead mb-4"></p>
         </div>
       </div>
-      {/* <div>
-        <Tripsidebar />
-      </div> */}
-    </>
   );
-  // }
 }
 
 export default MainPage;
--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -4,19 +4,83 @@ import { useToken, getToken } from "./Accounts/auth";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/">
+    <nav className="navbar navbar-expand-lg bg-dark">
+      <div className="container-fluid ">
+        <NavLink className="me-4 ms-1" to="/">
           <img
-            src="/docs/5.3/assets/brand/bootstrap-logo.svg"
+            src={require("./north_star_logo.png")}
             alt="Logo"
-            width="30"
-            height="24"
-            className="d-inline-block align-text-top"
+            width="50"
+            height="45"
+            className="d-flex align-content-center"
+            style={{
+              border:"1px solid white"
+            }}
           />
-          North Star
         </NavLink>
-        <div className="navbar-header">
+        <div
+          className="collapse navbar-collapse"
+          id="navbarNav"
+        >
+          <ul
+            className="navbar-nav"
+            style={{
+              width:"10%"
+            }}
+          >
+            <li
+              className="nav-item"
+            >
+              <NavLink
+                to="/"
+                className="text-white nav-link bg-blue rounded-pill"
+              >
+                North Star
+              </NavLink>
+            </li>
+            <li
+              className="nav-item"
+            >
+              <NavLink
+                to="/trips"
+                className="text-white nav-link"
+              >
+                Trips
+              </NavLink>
+            </li>
+          </ul>
+          <ul 
+            className="navbar-nav d-flex flex-row-reverse"
+            style={{
+              width:"90%"
+            }}
+          >
+            <li className="nav-item d-flex align-items-center">
+              <LogOut />
+            </li>
+            <li
+              className="nav-item "
+            >
+              <NavLink
+                to="/signup"
+                className="text-white nav-link"
+              >
+                Sign Up
+              </NavLink>
+            </li>
+            <li
+              className="nav-item "
+            >
+              <NavLink
+                to="/login"
+                className="text-white nav-link"
+              >
+                Login
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        {/* <div className="navbar-header">
           <NavLink className="nav-item text-3xl font-['Orbitron']" to="/">
             North Star
           </NavLink>
@@ -41,7 +105,7 @@ function Nav() {
           <li className="nav-item">
             <LogOut />
           </li>
-        </ul>
+        </ul> */}
       </div>
     </nav>
   );
