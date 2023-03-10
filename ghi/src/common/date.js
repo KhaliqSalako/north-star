2cdb7958520c67508c04579da4a7df30ec75c0dd
Braden Towns <bradenstowns@gmail.com.here>
2023-03-10 15:31:32 -0600

    Bug Fixed with Date Display

--- ghi/src/common/date.js
+++ ghi/src/common/date.js
@@ -1,6 +1,6 @@
 export default function toDateFormat(date) {
   const dateArr = date.split("-");
-  let wordDate = new Date(dateArr[0], dateArr[1], dateArr[2]);
+  let wordDate = new Date(dateArr[0], dateArr[1] - 1, dateArr[2]);
   wordDate = wordDate.toDateString().split(" ");
   wordDate[2] = wordDate[2] + ",";
   return (wordDate = wordDate.join(" "));
