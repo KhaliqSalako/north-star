507cd5dc91de05bbf0077b610e6a5fa4e4d6951d
Louis <pandaboy9902@gmail.com>
2023-03-15 15:51:57 -0400

    CI CD test24

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -22,7 +22,7 @@ function App() {
   }
 
   return (
-    <BrowserRouter basename="/north-star-project">
+    <BrowserRouter basename={"/north-star-project"}>
       <AuthProvider>
         <GetToken />
         <Nav />
