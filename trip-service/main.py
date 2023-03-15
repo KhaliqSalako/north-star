bcecfa195e7c89e233d6b7614291808a2cabc15c
Louis <pandaboy9902@gmail.com>
2023-03-15 15:01:12 -0400

    CI CD test21

--- trip-service/main.py
+++ trip-service/main.py
@@ -14,8 +14,7 @@ app.include_router(events.router, tags=["Events"])
 
 
 origins = [
-    # os.environ.get("CORS_HOST", "https://north-star-group.gitlab.io"),
-    os.environ.get("PUBLIC_URL", "http://localhost:3000"),
+    os.environ.get("CORS_HOST", "http://localhost:3000"),
 ]
 
 app.add_middleware(
